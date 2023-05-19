0f990f9fc9ad7c05f948fd4256f2f7afa75228d2
Chao Zhou <Czhou0k@gmail.com>
2023-05-19 14:30:39 -0400

    added more to app js component

--- src/App.js
+++ src/App.js
@@ -56,10 +56,49 @@ function App({ signOut }) {
 
   return (
     <View className='App'>
-      <Card>
-        <Image src={logo} className='App-logo' alt='logo' />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
+      <Heading level={1}>My Notes App</Heading>
+      <View as='form' margin='3rem 0' onSubmit={createNote}>
+        <Flex direction='row' justifyContent='center'>
+          <TextField
+            name='name'
+            placeholder='Note Name'
+            label='Note Name'
+            labelHidden
+            variation='quiet'
+            required
+          />
+          <TextField
+            name='description'
+            placeholder='Note Description'
+            label='Note Description'
+            labelHidden
+            variation='quiet'
+            required
+          />
+          <Button type='submit' variation='primary'>
+            Create Note
+          </Button>
+        </Flex>
+      </View>
+      <Heading level={2}>Current Notes</Heading>
+      <View margin='3rem 0'>
+        {notes.map((note) => (
+          <Flex
+            key={note.id || note.name}
+            direction='row'
+            justifyContent='center'
+            alignItems='center'
+          >
+            <Text as='strong' fontWeight={700}>
+              {note.name}
+            </Text>
+            <Text as='span'>{note.description}</Text>
+            <Button variation='link' onClick={() => deleteNote(note)}>
+              Delete note
+            </Button>
+          </Flex>
+        ))}
+      </View>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
